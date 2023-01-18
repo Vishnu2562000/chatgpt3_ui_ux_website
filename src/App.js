d5747e4319188cfafe717782a7def5b58bbf7aaf
Vishnu Vardhan Chandragiri <chandragirivishnuvardhan@gmail.com>
2023-01-18 14:50:16 +0530

    Implemented Navigation Bar component and it's styling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+/*
+Figma Design for app
+https://www.figma.com/file/lz9lLpFHMxHm2odnwM3R0z/gpt3?node-id=0%3A1&t=CT7Mb2xBuV6JTIYS-0
+*/
 import React from 'react';
 
 import { Footer, Blog, Possibility, WhatGPT3, Features, Header } from './containers';
